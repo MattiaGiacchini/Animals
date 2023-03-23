a10d2836ec531a418194fcb36fc3b0b22bd6ae0b
Mattia <giacchini.mattia@gmail.com>
2023-03-23 20:44:16 +0100

    header router link component

---
+++ src/components/uiHeaderLink.vue
@@ -0,0 +1,44 @@
+<template>
+  <router-link class="my-auto text-center text-base font-medium inline-block px-4 py-2 rounded-md " :to="to"
+    :class="`${type}`" exact>
+    <slot></slot>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: "uiHeaderRouterLink",
+  inheritAttrs: false,
+  props: {
+    type: {
+      type: String,
+      values: () => ["active", "default"],
+      default: "default",
+    },
+    to: {
+      type: Object,
+      default() {
+        return { name: "Home" };
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.default {
+  @apply text-white;
+
+  &:hover {
+    @apply bg-primary-dark;
+  }
+}
+
+.router-link-exact-active {
+  @apply bg-primary-light;
+
+  &:hover {
+    @apply bg-primary-dark;
+  }
+}
+</style>
