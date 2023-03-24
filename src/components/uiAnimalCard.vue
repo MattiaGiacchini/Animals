74ac805f022f995d4f8d9013465d3e7bfff82645
Mattia <giacchini.mattia@gmail.com>
2023-03-24 15:47:29 +0100

    display full information in animal card

--- src/components/uiAnimalCard.vue
+++ src/components/uiAnimalCard.vue
@@ -1,11 +1,19 @@
 <template>
-    <div class="rounded-md bg-white w-96 flex flex-col gap-1 p-6 text-lg font-medium">
-        <div class="text-3xl font-semibold bg-primary-ultralight rounded-md mb-2 p-2">{{ name }}</div>
-        <div class="flex flex-row justify-between">
-            <div>{{ type }}</div>
-            <div>{{ breed }}</div>
+    <router-link :to="`animals/${id}`"
+        class="rounded-md bg-white w-96 flex flex-col p-6 text-lg font-medium shadow-lg hover:cursor-pointer">
+        <div class="text-3xl font-semibold bg-primary-light/50 rounded-md mb-6 p-2">{{ name }}</div>
+        <div class="flex flex-col">
+            <div class="flex justify-start">Type: {{ type }}</div>
+            <div class="flex justify-start">Breed: {{ breed }}</div>
         </div>
-    </div>
+        <div class="flex flex-col">
+            <div class="flex justify-start">Gender: {{ gender }}</div>
+        </div>
+        <div class="flex flex-col">
+            <div class="flex justify-start">Vaccinated: {{ vaccinated ? "YES" : "NO" }}</div>
+            <div class="flex justify-start">Last visit: {{ new Date(lastVisit).toLocaleDateString("es-ES") }}</div>
+        </div>
+    </router-link>
 </template>
   
 <script>
@@ -20,6 +28,7 @@ export default {
         vaccinated: { type: Boolean, default: false },
         lastVisit: { type: String, default: null },
         lastUpdate: { type: String, default: null },
-    }
+    },
+
 };
 </script>
