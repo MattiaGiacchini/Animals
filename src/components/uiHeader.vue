9c5fe45441935b4e5aa992b1e01f79d5da214e54
Mattia <giacchini.mattia@gmail.com>
2023-03-24 10:47:16 +0100

    header: added shadow

--- src/components/uiHeader.vue
+++ src/components/uiHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="bg-primary flex fixed top-0 w-full mx-auto px-4 shadow-sm min-h-[4rem] max-h-16 items-center z-10 justify-between gap-8">
+        class="bg-primary flex fixed top-0 w-full mx-auto px-4 min-h-[4rem] max-h-16 items-center z-10 justify-between gap-8 shadow-md">
         <ui-logo class="flex w-max justify-start items-center cursor-pointer"
             @click="$router.push({ name: 'Home' }).catch(() => { })"></ui-logo>
         <div class="flex w-full justify-end gap-2">
