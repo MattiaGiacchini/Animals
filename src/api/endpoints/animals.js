85bcf90a3cb8d2c502f16eb99a19db89377717b0
Mattia <giacchini.mattia@gmail.com>
2023-03-25 20:06:23 +0100

    added functions for PUT, DELETE and POST

--- src/api/endpoints/animals.js
+++ src/api/endpoints/animals.js
@@ -1,5 +1,7 @@
+const baseUrl = "http://localhost:3000"
+
 export function getAllAnimals() {
-    return fetch("/api/animals")
+    return fetch(`${baseUrl}/api/animals`)
       .then(response => {
         return response.json();
       })
@@ -11,8 +13,61 @@ export function getAllAnimals() {
       });
   }
   
-  export function getAnimalById(id) {
-    return fetch(`/api/animals/${id}`)
+  export function getAnimalById(animalId) {
+    return fetch(`${baseUrl}/api/animals/${animalId}`)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        return data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  export function createAnimal(animalData) {
+    return fetch(`${baseUrl}/api/animals`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(animalData)
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        return data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  
+  export function updateAnimal(animalId, animalData) {
+    return fetch(`${baseUrl}/api/animals/${animalId}`, {
+      method: "PUT", 
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(animalData)
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        return data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  export function deleteAnimal(animalId) {
+    return fetch(`${baseUrl}/api/animals/${animalId}`, {
+      method: "DELETE"
+    })
       .then(response => {
         return response.json();
       })
