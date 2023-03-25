f1848b180c6fef1937d6824e0b1d6ff22b0d0b5d
Mattia <giacchini.mattia@gmail.com>
2023-03-25 14:43:56 +0100

    new eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,28 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended'
   ],
+  parser: "vue-eslint-parser",
   parserOptions: {
-    parser: '@babel/eslint-parser'
+    parser: '@babel/eslint-parser',
+    sourceType: "module"
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "vue/html-self-closing": ["error", {
+      "html": {
+        "void": "always",
+        "normal": "never",
+        "component": "never"
+      }
+    }],
+    "vue/attribute-hyphenation": [ "never", {
+      "ignore": []
+    }]
   },
   overrides: [
     {
