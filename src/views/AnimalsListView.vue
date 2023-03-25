caa2d12315abd26198f338ce34b4617c976ebfc5
Mattia <giacchini.mattia@gmail.com>
2023-03-25 14:17:31 +0100

    animal list refactor - grid instead of flex

--- src/views/AnimalsListView.vue
+++ src/views/AnimalsListView.vue
@@ -1,26 +1,37 @@
 <template>
-    <div class="flex flex-row w-screen h-full">
-        <ui-elements-list class="w-4/5"></ui-elements-list>
-        <ui-side-bar class="w-1/5">
-            <div slot="subtitle">Filtros</div>
-            <ui-animals-filters :animalsCategories="[]" slot="main-content"></ui-animals-filters>
-            <div slot="bottom-controls" class="flex flex-col gap-2">
-
-                <div class="flex">
-                    <ui-button :loading="false" type="primary">Create new</ui-button>
-
-                </div>
-            </div>
-        </ui-side-bar>
-    </div>
+  <div class="flex flex-row w-screen h-full">
+    <ui-elements-list class="w-4/5"></ui-elements-list>
+    <ui-side-bar class="w-1/5">
+      <div slot="subtitle">
+        Filtros
+      </div>
+      <ui-animals-filters
+        slot="main-content"
+        :animalsCategories="[]"
+      ></ui-animals-filters>
+      <div
+        slot="bottom-controls"
+        class="flex flex-col gap-2"
+      >
+        <div class="flex">
+          <ui-button
+            :loading="false"
+            type="primary"
+          >
+            Create new
+          </ui-button>
+        </div>
+      </div>
+    </ui-side-bar>
+  </div>
 </template>
   
 <script>
 // @ is an alias to /src
 import uiElementsList from '@/components/uiElementsList.vue';
-import uiSideBar from '@/components/uiSideBar.vue';
+import uiSideBar from '@/components/uiGeneral/uiSideBar.vue';
 import uiAnimalsFilters from '@/components/uiAnimalsFilters.vue';
-import uiButton from '@/components/uiButton.vue'
+import uiButton from '@/components/uiGeneral/uiButton.vue'
 
 export default {
     name: 'AnimalsListView',
