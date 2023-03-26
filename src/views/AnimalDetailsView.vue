f72faad097f841245540f27aea6460c9da87ac40
Mattia <giacchini.mattia@gmail.com>
2023-03-26 10:55:38 +0200

    renamed view

--- src/views/AnimalDetailsView.vue
+++ src/views/AnimalDetailsView.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="about flex flex-row">
-    <h1 class="w-4/5">
-      This is an about page
-    </h1>
+    <h1 class="w-4/5">This is an about page</h1>
     <ui-animal-data-modal
       v-if="displayModal"
       @closeModal="displayModal = false"
-      :animalData=animalData
+      :animalData="animalData"
     ></ui-animal-data-modal>
     <ui-side-bar class="w-1/5">
-
       <ui-animals-filters
         slot="main-content"
         :animalsCategories="[]"
@@ -19,13 +16,16 @@
         class="flex flex-col gap-5"
       >
         <div class="flex flex-row gap-5">
-          <ui-button :loading="false">
+          <ui-button
+            :loading="false"
+            @click="deleteAnimal()"
+          >
             Delete
           </ui-button>
           <ui-button
             :loading="false"
             type="primary"
-            @click="displayModal=true"
+            @click="displayModal = true"
           >
             Edit
           </ui-button>
@@ -36,37 +36,41 @@
 </template>
 
 <script>
-import uiSideBar from '@/components/uiGeneral/uiSideBar.vue';
-import uiAnimalsFilters from '@/components/uiAnimalsFilters.vue';
-import uiButton from '@/components/uiGeneral/uiButton.vue'
-import uiAnimalDataModal from '@/components/uiAnimalDetails/uiAnimalDataModal.vue';
-import { getAnimalById } from '@/api/endpoints/animals';
+import uiSideBar from "@/components/uiGeneral/uiSideBar.vue";
+import uiAnimalsFilters from "@/components/uiAnimalsFilters.vue";
+import uiButton from "@/components/uiGeneral/uiButton.vue";
+import uiAnimalDataModal from "@/components/uiAnimalDetails/uiAnimalDataModal.vue";
+import { getAnimalById, deleteAnimal } from "@/api/endpoints/animals";
 
 export default {
+  name: "AnimalDetailsView",
   components: {
     "ui-side-bar": uiSideBar,
     "ui-animals-filters": uiAnimalsFilters,
     "ui-button": uiButton,
-    "ui-animal-data-modal": uiAnimalDataModal
+    "ui-animal-data-modal": uiAnimalDataModal,
   },
   data() {
     return {
       displayModal: false,
-      animalData: null
-    }
+      animalData: null,
+    };
   },
   created() {
     const id = this.$route.params.animal;
-    console.log(id)
     getAnimalById(id)
-      .then(data => {
+      .then((data) => {
         this.animalData = data;
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
-  }
-
-
-}
+  },
+  methods: {
+    deleteAnimal() {
+      deleteAnimal(this.$route.params.animal);
+      this.$router.push({ name: "AnimalsList" }).catch(() => {});
+    },
+  },
+};
 </script>
