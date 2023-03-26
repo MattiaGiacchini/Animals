d76e90e72958175be686845df3ddca23e3c6eab4
Mattia <giacchini.mattia@gmail.com>
2023-03-26 10:55:19 +0200

    fixed container height in animalList

--- src/components/uiAnimalsList.vue
+++ src/components/uiAnimalsList.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="grid grid-cols-3 xl:grid-cols-4 gap-5 p-5 justify-center overflow-y-auto scrollbar">
+  <div
+    class="max-h-full h-fit grid grid-cols-3 xl:grid-cols-4 gap-5 p-5 justify-center overflow-y-auto scrollbar"
+  >
     <ui-animal-card
       v-for="(animal, index) in animals"
       :animalData="animal"
@@ -10,34 +12,31 @@
     </ui-animal-card>
   </div>
 </template>
-  
+
 <script>
-import uiAnimalCard from '@/components/uiAnimalCard.vue';
-import {getAllAnimals} from "@/api/endpoints/animals"
+import uiAnimalCard from "@/components/uiAnimalCard.vue";
+import { getAllAnimals } from "@/api/endpoints/animals";
 
 export default {
-    name: "UiAnimalsList",
-    components: {
-        "ui-animal-card": uiAnimalCard
-    },
-    data() {
-        return {
-            loading: false,
-            info: null,
-            animals: []
-
-        }
-    },
-    created() {
-        getAllAnimals()
-        .then(data => {
-            this.animals = data;
-        })
-        .catch(error => {
-            console.log(error);
-        });
-    }
+  name: "UiAnimalsList",
+  components: {
+    "ui-animal-card": uiAnimalCard,
+  },
+  data() {
+    return {
+      loading: false,
+      info: null,
+      animals: [],
+    };
+  },
+  created() {
+    getAllAnimals()
+      .then((data) => {
+        this.animals = data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
 };
 </script>
-  
-
