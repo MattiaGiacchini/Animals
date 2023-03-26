8b9b5d91685e7ab924439b329d9e2ffbc735c3e9
Mattia <giacchini.mattia@gmail.com>
2023-03-26 10:54:24 +0200

    added delete function to endpoint

--- src/api/endpoints/animals.js
+++ src/api/endpoints/animals.js
@@ -1,81 +1,80 @@
-const baseUrl = "http://localhost:3000"
+const baseUrl = "http://localhost:3000";
 
 export function getAllAnimals() {
-    return fetch(`${baseUrl}/api/animals`)
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        return data;
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-  
-  export function getAnimalById(animalId) {
-    return fetch(`${baseUrl}/api/animals/${animalId}`)
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        return data;
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
+  return fetch(`${baseUrl}/api/animals`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
-  export function createAnimal(animalData) {
-    return fetch(`${baseUrl}/api/animals`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(animalData)
+export function getAnimalById(animalId) {
+  return fetch(`${baseUrl}/api/animals/${animalId}`)
+    .then((response) => {
+      return response.json();
     })
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        return data;
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-  
-  export function updateAnimal(animalId, animalData) {
-    return fetch(`${baseUrl}/api/animals/${animalId}`, {
-      method: "PUT", 
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(animalData)
+    .then((data) => {
+      return data;
     })
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        return data;
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
+    .catch((error) => {
+      console.log(error);
+    });
+}
 
-  export function deleteAnimal(animalId) {
-    return fetch(`${baseUrl}/api/animals/${animalId}`, {
-      method: "DELETE"
+export function createAnimal(animalData) {
+  return fetch(`${baseUrl}/api/animals`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(animalData),
+  })
+    .then((response) => {
+      return response.json();
     })
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        return data;
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-  
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+export function updateAnimal(animalId, animalData) {
+  return fetch(`${baseUrl}/api/animals/${animalId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(animalData),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+export function deleteAnimal(animalId) {
+  return fetch(`${baseUrl}/api/animals/${animalId}`, {
+    method: "DELETE",
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
