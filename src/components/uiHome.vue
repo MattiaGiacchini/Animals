13fd2c9be941b2d15eec82aee68ed6081e4ec2b3
Mattia <giacchini.mattia@gmail.com>
2023-03-27 10:00:24 +0200

    created home component

---
+++ src/components/uiHome.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="flex w-full h-full mx-auto px-4 items-center justify-center flex-col gap-5"
+  >
+    <div
+      class="flex justify-center items-center gap-5 p-12 flex-col m-auto rounded-md bg-white shadow-lg cursor-pointer z-10 hover:shadow-xl max-w-xl"
+      @click="$router.push({ name: 'AnimalsList' }).catch(() => {})"
+    >
+      <ui-logo
+        class="flex w-max justify-start items-center"
+        fill-color="#3c9700"
+      ></ui-logo>
+      <h2 class="text-xl">
+        Welcome to <span class="italic font-semibold">Animals</span> platform!
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import UiLogo from "@/components/uiGeneral/uiLogo.vue";
+
+export default {
+  name: "uiHeader",
+  components: {
+    "ui-logo": UiLogo,
+  },
+};
+</script>
