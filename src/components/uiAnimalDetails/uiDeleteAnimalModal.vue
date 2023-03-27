5e256a5457539a7abcf1751b1240fc66a5f50c68
Mattia <giacchini.mattia@gmail.com>
2023-03-27 13:06:07 +0200

    created modal to confirm deletion

---
+++ src/components/uiAnimalDetails/uiDeleteAnimalModal.vue
@@ -0,0 +1,45 @@
+<template>
+  <ui-modal
+    title="Delete Animal"
+    :primaryButtonLoading="loading"
+    primaryButtonText="Confirm"
+    secondaryButtonText="Cancel"
+    @closeModal="closeModal"
+    @primaryClick="deleteAnimal"
+    @secondaryClick="closeModal"
+    primaryButtonType="danger"
+  >
+    <div class="flex flex-row gap-5 w-full">
+      Are you sure you want to delete this animal?
+    </div>
+  </ui-modal>
+</template>
+
+<script>
+import uiModal from "@/components/uiGeneral/uiModal.vue";
+import { deleteAnimal } from "@/api/endpoints/animals";
+
+export default {
+  name: "uiDeleteAnimalModal",
+  components: {
+    "ui-modal": uiModal,
+  },
+  props: {
+    loading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+
+  methods: {
+    closeModal() {
+      this.$emit("closeModal");
+    },
+    deleteAnimal() {
+      deleteAnimal(this.$route.params.animal).then(() =>
+        this.$router.push({ name: "AnimalsList" }).catch(() => {})
+      );
+    },
+  },
+};
+</script>
